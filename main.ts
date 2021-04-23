8090abb2df5154b104eda9ac2e66e926bedf1abb
3007592 <79598031+3007592@users.noreply.github.com>
2021-04-23 12:00:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 let value = 0
 basic.forever(function () {
     value = pins.digitalReadPin(DigitalPin.P1)
-    if (pins.digitalReadPin(DigitalPin.P1) < 200) {
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P1))
+    if (pins.digitalReadPin(DigitalPin.P1) < 1) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(300)
         pins.digitalWritePin(DigitalPin.P0, 0)
